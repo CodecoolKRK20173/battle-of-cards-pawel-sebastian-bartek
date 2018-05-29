2eef49370289fb890089d2d3590af637d90cf01f
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-05-29 04:27:35 +0200

    add pool, constructor, and method

--- Suits.java
+++ Suits.java
@@ -1,4 +1,15 @@
 public enum Suits{
-    CLUBS ('\u2663'), DIAMONDS('\u2662'), HEARTS('\u2661'), SPADES('\u2660')
+    CLUBS ('\u2663'), DIAMONDS('\u2662'), HEARTS('\u2661'), SPADES('\u2660');
+    
+    
+    private char graphicalRepresentation;
+    
+    private Suits(char representation){
+        this.graphicalRepresentation = representation;
+    }
+        
+    public String getRepresentation(){
+        return String.valueOf(graphicalRepresentation);
+    }
 }
     

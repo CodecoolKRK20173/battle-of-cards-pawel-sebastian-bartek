ffa738e5cb51a1825705db0a5d22fcf682c26353
Paweł Popowicz <p.p.popowicz@gmail.com>
2018-06-01 11:52:04 +0200

    add clearScreen()

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        clearScreen();
         new Game().gameHandler();
+    }
+
+    private static void clearScreen(){
+        //Runtime.getRuntime().exec("clear"); // nie działa, więc wytrychem go:
+        System.out.print("\033[H\033[2J");  
+        System.out.flush(); 
     }
 }
